4f8e8630df97e22614a6173788dc0b86310717a9
Nelliesohl <nelliesohl@icloud.com>
2025-01-13 20:41:17 +0000

    Feat: Setup google sheets api connection

--- run.py
+++ run.py
@@ -2,3 +2,16 @@ from datetime import datetime
 
 import gspread
 from google.oauth2.service_account import Credentials
+
+
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SPREADSHEET = GSPREAD_CLIENT.open('to_do_list')
+WORKSHEET = SPREADSHEET.worksheet('tasks')
